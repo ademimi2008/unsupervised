c086b137bb4da1486a065cf32d9d00d912b547ba
ademimi2008 <ademimi2008@gmail.com>
2026-04-17 09:56:25 +0600

    Delete cs_22-master directory

--- cs_22-master/cs_22-master/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import AccountAPIView, AccountPostAPIView
-
-
-urlpatterns = [
-    path('', AccountAPIView.as_view(), name='accounts'),
-    path('<int:pk>', AccountPostAPIView.as_view(), name='account-posts')
-]
